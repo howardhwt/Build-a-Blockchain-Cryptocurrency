95952b334dc4ca255847e830b6e0edc352133fcd
Howard Tam <howard.tam.hw@gmail.com>
2022-10-26 00:20:16 -0400

    blocktest added

--- cryptochain/block.js
+++ cryptochain/block.js
@@ -1,11 +1,13 @@
 class Block {
-  constructor({ timestamp, lastHash, hash, data }) {
-    this.timestamp = timestamp;
-    this.lastHash = lastHash;
-    this.hash = hash;
-    this.data = data;
-  }
+  // constructor({ timestamp, lastHash, hash, data }) {
+  //   this.timestamp = timestamp;
+  //   this.lastHash = lastHash;
+  //   this.hash = hash;
+  //   this.data = data;
+  // }
 }
+
+module.exports = Block;
 
 const block1 = new Block({
   timestamp: "01/01/01",
---
+++ cryptochain/block.test.js
@@ -0,0 +1,14 @@
+const Block = require("./block");
+
+describe("Block", () => {
+  const timestamp = "a-date";
+  const lastHash = "foo-hash";
+  const hash = "bar-hash";
+  const data = ["blockchain", "data"];
+  const block = new Block({
+    timestamp,
+    lastHash,
+    hash,
+    data,
+  });
+});
